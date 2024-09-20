03beb89377973ca46a62a9341d54e72478958275
nayon117 <hasibul.nayon1@gmail.com>
2024-09-20 09:03:23 +0600

    add token

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout><Home/></Layout>} />
             <Route path="*" element={<div><Home/></div>} />
-            <Route path="user-profile" element={<div>user Profile</div>} />
+            <Route path="/user-profile" element={<div>user Profile</div>} />
+            <Route path="/auth-callback" element={<AuthCallbackPage />} />
 
         </Routes>
     )
--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,13 +1,12 @@
-import { useCreateMyUser } from "@/api/UserApi";
-import { AppState, Auth0Provider, User} from "@auth0/auth0-react";
+import { AppState, Auth0Provider} from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-
-  const {createUser} = useCreateMyUser();
+  const navigate = useNavigate();
 
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -18,10 +17,9 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("Missing Auth0 environment variables");
   }
 
-  const onRedirectCallback = (appState?: AppState,user?:User) => {
-    if(user?.sub && user?.email){
-      createUser({auth0Id:user.sub,email:user.email})
-    }
+  const onRedirectCallback = (appState?: AppState) => {
+    // navigate(appState?.returnTo || window.location.pathname);
+    navigate('/auth-callback');
   };
 
 
