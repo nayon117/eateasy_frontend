39c0c394a20a1b8de3863ee47f854a0bac6dcb53
nayon117 <hasibul.nayon1@gmail.com>
2024-09-22 09:50:34 +0600

    create profile form

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./pages/Home";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Layout><Home/></Layout>} />
+            <Route path="/" element={<Layout showHero ><Home/></Layout>} />
             <Route path="*" element={<div><Home/></div>} />
-            <Route path="/user-profile" element={<div>user Profile</div>} />
+            <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
             <Route path="/auth-callback" element={<AuthCallbackPage />} />
 
         </Routes>
---
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,116 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  email: z.string().optional(),
+  name: z.string().min(1, "Name is required"),
+  addressLine1: z.string().min(1, "addressLine1 is required"),
+  country: z.string().min(1, "country is required"),
+  city: z.string().min(1, "city is required"),
+});
+
+type userFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  onSave: (userProfileData: userFormData) => void;
+  isLoading: boolean;
+};
+
+const UserProfileForm = ({ onSave, isLoading }: Props) => {
+  const form = useForm<userFormData>({
+    resolver: zodResolver(formSchema),
+  });
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSave)}
+        className="bg-gray-100 rounded-lg space-y-4 md:p-10"
+      >
+        <div>
+          <h2 className="text-2xl font-bold text-teal-600 ">Profile</h2>
+          <FormDescription>view and edit your profile</FormDescription>
+        </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} disabled className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <div className="flex flex-col md:flex-row gap-4">
+        <FormField
+          control={form.control}
+          name="addressLine1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>AddressLine1</FormLabel>
+              <FormControl>
+                <Input {...field} className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="city"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>City</FormLabel>
+              <FormControl>
+                <Input {...field} className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="country"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Country</FormLabel>
+              <FormControl>
+                <Input {...field} className="bg-white" />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        </div>
+
+          {
+            isLoading ? <LoadingButton /> : <Button type="submit" className="bg-teal-500">Submit</Button>
+          }
+
+      </form>
+    </Form>
+  );
+};
+export default UserProfileForm;
